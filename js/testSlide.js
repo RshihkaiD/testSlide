d6c109f477233b47748c95eb0200adeb0b629058
RshihkaiD-SourceTree <kevin.chang@tgc-taiwan.com.tw>
2017-05-16 11:45:03 +0800

    testSlideShow

---
+++ js/testSlide.js
@@ -0,0 +1,15 @@
+/**
+ * Created by litv-frontend on 2017/5/16.
+ */
+
+var TestSlide = new(function () {
+    this.slideInit = function (param) {
+
+    }
+})();
+
+var SendParam = {
+    dom:'[data-slide-block]',
+    speed:
+};
+TestSlide.slideInit();
