13f26bd8d6d0a1715ebd7a545a088eb09593d80b
RshihkaiD-SourceTree <kevin.chang@tgc-taiwan.com.tw>
2017-05-16 11:48:24 +0800

    no message

--- js/testSlide.js
+++ js/testSlide.js
@@ -2,14 +2,19 @@
  * Created by litv-frontend on 2017/5/16.
  */
 
-var TestSlide = new(function () {
+var TestSlide = new (function () {
     this.slideInit = function (param) {
 
     }
 })();
 
 var SendParam = {
-    dom:'[data-slide-block]',
-    speed:
+    dom: '[data-slide-block]',
+    speed: 5000,
+    data: {
+        file: 'data.json',
+        type:'file'// is file || Json string || Json Object
+    },
+    gaName:''
 };
 TestSlide.slideInit();
